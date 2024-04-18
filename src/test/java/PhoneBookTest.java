7327626d58b730a4b34ce58cd3b4d53d552a0811
dnkarider <prodanila1234@mail.ru>
2024-04-18 11:57:18 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 public class PhoneBookTest {
+    @Test
+    public void testAdd(){
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Michael", "+78020200299");
+        phoneBook.add("Michael", "+78228828828");
+        Assertions.assertTrue(phoneBook.add("Michael", "+78228828828") == 1);
+    }
 }

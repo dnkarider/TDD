329dce84d1fafeaff689bc0289b5979643b15a57
dnkarider <prodanila1234@mail.ru>
2024-04-18 12:14:42 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private static PhoneBook PHONEBOOK = null;
+    private static Map<String, String> book = new HashMap<>();
+    private PhoneBook(){}
     public int add(String name, String number){
-        return 0;
+        book.put(name, number);
+        return book.size();
+    }
+    public static PhoneBook getInstance(){
+        if(PHONEBOOK == null){
+            synchronized (PhoneBook.class){
+                if(PHONEBOOK == null){
+                    PHONEBOOK= new PhoneBook();
+                }
+            }
+        }
+        return PHONEBOOK;
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 public class PhoneBookTest {
     @Test
     public void testAdd(){
-        PhoneBook phoneBook = new PhoneBook();
+        PhoneBook phoneBook = PhoneBook.getInstance();
         phoneBook.add("Michael", "+78020200299");
         phoneBook.add("Michael", "+78228828828");
         Assertions.assertTrue(phoneBook.add("Michael", "+78228828828") == 1);

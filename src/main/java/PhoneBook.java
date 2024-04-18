07edde271b6cc781b64e3728cb8b04bda0ae8b85
dnkarider <prodanila1234@mail.ru>
2024-04-18 14:28:39 +0300

    Stub and tests for printAllNames

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -28,6 +29,8 @@ public class PhoneBook {
             number = book.get(name);
         }
         return number;
+    }
+    public void printAllNames(){
     }
     public static PhoneBook getInstance(){
         if(PHONEBOOK == null){
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,5 +1,10 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class PhoneBookTest {
     @Test
     public void testAdd(){
@@ -21,5 +26,25 @@ public class PhoneBookTest {
         phoneBook.add("Michael", "+78020200299");
         phoneBook.add("Хасбула", "+79156666666");
         Assertions.assertEquals("+79156666666", phoneBook.findByName("Хасбула"));
+    }
+    @Test
+    public void testPrintAllNames(){
+        PhoneBook phoneBook = PhoneBook.getInstance();
+        phoneBook.add("Michael", "+78020200299");
+        phoneBook.add("Хасбула", "+79156666666");
+        String consoleOutput = null;
+        PrintStream originalOut = System.out;
+        try{
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream(100);
+            PrintStream capture = new PrintStream(outputStream);
+            System.setOut(capture);
+            phoneBook.printAllNames();
+            capture.flush();
+            consoleOutput = outputStream.toString();
+            System.setOut(originalOut);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        Assertions.assertEquals("Хасбула Michael ", consoleOutput);
     }
 }
